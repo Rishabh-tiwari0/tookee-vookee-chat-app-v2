6265a41cfe47371d9a82dc403a9fb15b7e465192
Rishabh-tiwari0 <htrishabh.tiwari@gmail.com>
2024-02-01 22:37:41 +0530

    token persistance update

--- Backend/middleware/authenticationMiddleware.js
+++ Backend/middleware/authenticationMiddleware.js
@@ -11,6 +11,8 @@ const protect = asyncHandler(async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
+      // Save the token in localStorage for persistence
+      localStorage.setItem("token", token);
 
       //decodes token id
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
